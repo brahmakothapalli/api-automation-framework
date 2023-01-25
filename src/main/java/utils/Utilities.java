ee96c4eccab3017f73d06f1d683e4e69a791844f
brahmakothapalli <brahma457@gmail.com>
2023-01-25 11:03:12 +0530

    created reusable post call method and updated the create airline test

---
+++ src/main/java/utils/Utilities.java
@@ -0,0 +1,21 @@
+package utils;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+public class Utilities {
+
+    public static Response postRequest(String endPoint, String payload, Map<String, String> headers){
+
+        return RestAssured.given()
+                .baseUri(endPoint)
+                .headers(headers)
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .post().then().log().all().extract().response();
+
+    }
+}
--- src/test/java/airlines/AirlineTests.java
+++ src/test/java/airlines/AirlineTests.java
@@ -1,4 +1,33 @@
 package airlines;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.Utilities;
+
+import java.util.HashMap;
+
 public class AirlineTests {
+
+    @Test
+    public void createAirline(){
+
+        String endPoint = "https://api.instantwebtools.net/v1/airlines";
+        String payload = "{\n" +
+                "    \"id\": 12564878,\n" +
+                "    \"name\": \"Sri Lankan Airways\",\n" +
+                "    \"country\": \"Sri Lanka\",\n" +
+                "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
+                "    \"slogan\": \"From Sri Lanka\",\n" +
+                "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
+                "    \"website\": \"www.srilankaairways.com\",\n" +
+                "    \"established\": \"1990\"\n" +
+                "}";
+
+        Response response = Utilities.postRequest(endPoint, payload, new HashMap<>());
+        Assert.assertEquals(200, response.statusCode());
+
+    }
 }
